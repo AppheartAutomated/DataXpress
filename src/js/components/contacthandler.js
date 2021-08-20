d393da4664079775802f6a483831b3d41b33dd3f
wegiveyoubrand <automatedcafe@gmail.com>
2021-08-20 05:57:28 +0100

    Updated

--- src/js/components/contacthandler.js
+++ src/js/components/contacthandler.js
@@ -1,5 +1,8 @@
 const contactHandler = document.querySelector(".contactHandler"),
-  contactOptions = document.querySelectorAll(".contact-option");
+  contactOptions = document.querySelectorAll(".contact-option"),
+  contactOptionResponses = document.querySelectorAll(
+    ".contact .contact-content"
+  );
 
 function showContact() {
   document.querySelector(".contact-wrapper").classList.toggle("active");
@@ -11,11 +14,23 @@ contactHandler.addEventListener("click", (e) => {
   showContact();
 });
 
-contactOptions.forEach((contactOption) => {
-  contactOption.addEventListener("click", (e) => {
-    contactOptions.forEach((contactOption) => {
-      contactOption.classList.remove("active");
-    });
-    contactOption.classList.add("active");
+for (let i = 0; i < contactOptions.length; i++) {
+  contactOptions[i].addEventListener("click", function () {
+    for (let j = 0; j < contactOptions.length; j++) {
+      contactOptions[j].classList.remove("active");
+    }
+    this.classList.add("active");
+
+    let dataFilter = this.getAttribute("data-option");
+
+    for (let k = 0; k < contactOptionResponses.length; k++) {
+      contactOptionResponses[k].classList.remove("active");
+      contactOptionResponses[k].classList.add("hide");
+
+      if (contactOptionResponses[k].getAttribute("data-response") == dataFilter) {
+        contactOptionResponses[k].classList.remove("hide");
+        contactOptionResponses[k].classList.add("active");
+      }
+    }
   });
-});
+}
